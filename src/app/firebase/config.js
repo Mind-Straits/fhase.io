cde580c44daa57b9600a70b9026517e98611baeb
Ahad <86792385+Ahad-Haque@users.noreply.github.com>
2024-05-28 23:22:09 +0600

    firebase config

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -2,6 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+// firebaseConfig
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
